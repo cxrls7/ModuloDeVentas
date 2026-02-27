e5541f5341152a322b661ec856b0da3b7313d8c9
Carlos Daniel <carlitos17abx@gmail.com>
2026-02-27 15:09:54 -0500

    Cree los tests para realizar las pruebas antes de subir el programa a main

---
+++ tests/test_resgistro.py
@@ -0,0 +1,23 @@
+#Este es el test de registro, se encarga de probar el proceso de registro de un nuevo usuario en la aplicación. 
+#Importare como en el de validacion solo las funciones necesarias para las pruebas, no todo el programa.
+
+import unittest
+from src.models.ventas import ventas
+
+
+class TestRegistro(unittest.TestCase):
+
+    def test_creacion_venta(self):
+        venta = venta("Camisa", 2, 25.0, True)
+
+        self.assertEqual(venta.producto, "Camisa")
+        self.assertEqual(venta.cantidad, 2)
+        self.assertEqual(venta.precio_unitario, 25.0)
+        self.assertTrue(venta.es_vip)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+        
