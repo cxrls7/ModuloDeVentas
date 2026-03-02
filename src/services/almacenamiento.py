e8fcd2405ceb8891529c8663e2ff68746378499c
Carlos Molina <carlitos17abx@gmail.com>
2026-03-02 13:57:14 -0500

    Cree el almacenamito del prorgama

---
+++ src/services/almacenamiento.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+RUTA_ARCHIVO = os.path.join(os.path.dirname(__file__),"../data/ventas.json")
+
+def obtener_ventas():
+    "Devuelve la lista de ventas guardadas. Si el archivo no existe o esta vacio, devuelve una lista vacia."
+    if not os.path.exists(RUTA_ARCHIVO):
+       return[]
+
+    try:
+        with open(RUTA_ARCHIVO, "r") as archivo:
+            return json.load(archivo)
+    except json.JSONDecodeError:
+        return []
+    
+
+def guardar_venta(venta_dict):
+    "Guarda una nueva venta en el archivo JSON."
+    ventas = obtener_ventas()
+    ventas.append(venta_dict)
+
+    with open(RUTA_ARCHIVO, "w") as archivo:
+        json.dump(ventas, archivo, indent=4)
+        
+        
+
