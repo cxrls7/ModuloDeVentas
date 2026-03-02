5328bd1a5922e41ebc66db51d26c8be976038436
Carlos Molina <carlitos17abx@gmail.com>
2026-03-02 15:38:53 -0500

    fix

--- src/models/ventas.py
+++ src/models/ventas.py
@@ -8,9 +8,20 @@ class Venta:
         self.cantidad = cantidad
         self.precio_unitario = precio_unitario
         self.es_vip = es_vip
+
         self.subtotal = 0
         self.descuento = 0
         self.total = 0
-        
-    
-        pass
+
+        self.calcular_totales()  # 🔥 IMPORTANTE
+
+
+    def calcular_totales(self):
+        self.subtotal = self.cantidad * self.precio_unitario
+
+        if self.es_vip:
+            self.descuento = self.subtotal * 0.10
+        else:
+            self.descuento = 0
+
+        self.total = self.subtotal - self.descuento
