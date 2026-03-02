938b80d2511e30ed323e15823a700d8e6228dba9
Carlos Molina <carlitos17abx@gmail.com>
2026-03-02 15:21:48 -0500

    hice un arreglo

--- src/services/almacenamiento.py
+++ src/services/almacenamiento.py
@@ -2,7 +2,7 @@ import json
 import os
 
 RUTA_ARCHIVO = os.path.join(os.path.dirname(__file__),"../data/ventas.json")
-
+print("Ruta del archivo:", os.path.abspath(RUTA_ARCHIVO))
 def obtener_ventas():
     "Devuelve la lista de ventas guardadas. Si el archivo no existe o esta vacio, devuelve una lista vacia."
     if not os.path.exists(RUTA_ARCHIVO):
@@ -22,6 +22,6 @@ def guardar_venta(venta_dict):
 
     with open(RUTA_ARCHIVO, "w") as archivo:
         json.dump(ventas, archivo, indent=4)
-        
+
         
 
